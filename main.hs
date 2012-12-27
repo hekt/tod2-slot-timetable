00f6517665cddbba5514a11db7e51cee24bc7f69
hekt <hektorg@gmail.com>
2012-12-27 19:51:11 +0900

    Work

--- main.hs
+++ main.hs
@@ -1,15 +1,22 @@
 import System.IO
+import System.Environment (getArgs)
 import Text.Printf
 import Text.Regex (mkRegex, splitRegex)
 import Data.Maybe (fromJust, isJust)
 
 type SlotTable = [(Int, [String])]
 
+main = do
+  (time, slot) <- fmap parseArgs getArgs
+  timeTable slot time
+
 timeTable slot time = do
   withFile "slot_table_en.csv" ReadMode $ \handle -> do
-      table <- fmap parse $ hGetContents handle
-      let mins = time2mins (h, m)
-          s = (fromJust $ getSlotNumber slot table) - 1
+      table <- fmap parseCsv $ hGetContents handle
+      let mins  = time2mins time
+          s_    = getSlotNumber slot table
+          s     = fromJust (if isJust s_ then s_ 
+                            else error "invalid slot") - 1
           times = map mins2time $ scanl (+) mins $ repeat 10
           slots = drop s $ cycle table
       mapM_ (putStrLn . formatting) . take 88 $ zip times slots
@@ -19,10 +26,15 @@ formatting ((h,m), (n, slots)) =
     let slots' = concatMap (printf "%-12s") slots :: String
     in printf "%3d:%02d  [%2d]  %s" h m n slots'
 
-parse :: String -> SlotTable
-parse str = map (g . f) $ lines str
+parseCsv :: String -> SlotTable
+parseCsv str = map (g . f) $ lines str
     where f = splitRegex (mkRegex ",[ \t]*")
           g (x:xs) = (read x, xs)
+
+parseArgs :: [String] -> ((Int, Int), String)
+parseArgs (time:slot:_) = let (h, m_) = break (==':') time
+                              m       = tail m_
+                          in ((read h, read m), slot)
 
 getSlotNumber :: String -> [(Int, [String])] -> Maybe Int
 getSlotNumber _ []            = Nothing
